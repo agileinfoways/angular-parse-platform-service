0094952bc29809169d9525fa6fb423465aff03dd
kapil barad <baradkapil83@gmail.com>
2015-07-18 12:40:26 +0530

    added module

--- service.js
+++ service.js
@@ -1,4 +1,4 @@
-angular.module('app').service('ParseService', function(){
+angular.module('ngParseModule', []).service('ParseService', function(){
 	//App keys
 	var app_id = "YOUR_APP_ID";
 	var js_key = "YOUR_JAVASCRIPT_KEY";
